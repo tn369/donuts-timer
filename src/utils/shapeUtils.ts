a2cbf881f0cd0204cdf2d3051f948259980bbbe7
nakatomi <36021243+tn369@users.noreply.github.com>
2026-01-27 16:51:17 +0900

    refactor heart path utilities

--- src/utils/shapeUtils.ts
+++ src/utils/shapeUtils.ts
@@ -1,6 +1,7 @@
 /**
  * 形状計算のためのクラスとユーティリティ
  */
+import { approximateHeartPerimeter, getHeartPath, HEART_PERIMETER_FACTOR } from './heartPath';
 
 export interface Point {
   x: number;
@@ -222,14 +223,9 @@ export class HeartRenderer extends PathRenderer {
   constructor(size: number, strokeWidth: number) {
     super(size, strokeWidth);
     this.ticksCount = 4;
-    this.path = this.getHeartPath(size);
-    this.outerPath = this.getHeartPath(size + 2);
-    this.perimeter = 2 * Math.PI * this.radius * 1.2;
-  }
-
-  private getHeartPath(size: number): string {
-    const scale = size / 24;
-    return `M ${12 * scale} ${21.35 * scale} l ${-1.45 * scale} ${-1.32 * scale} C ${5.4 * scale} ${15.36 * scale} ${2 * scale} ${12.28 * scale} ${2 * scale} ${8.5 * scale} C ${2 * scale} ${5.42 * scale} ${4.42 * scale} ${3 * scale} ${7.5 * scale} ${3 * scale} c ${1.74 * scale} 0 ${3.41 * scale} ${0.81 * scale} ${4.5 * scale} ${2.09 * scale} C ${13.09 * scale} ${3.81 * scale} ${14.76 * scale} ${3 * scale} ${16.5 * scale} ${3 * scale} C ${19.58 * scale} ${3 * scale} ${22 * scale} ${5.42 * scale} ${22 * scale} ${8.5 * scale} c 0 ${3.78 * scale} ${-3.4 * scale} ${6.86 * scale} ${-8.55 * scale} ${11.54 * scale} L ${12 * scale} ${21.35 * scale} z`;
+    this.path = getHeartPath(size);
+    this.outerPath = getHeartPath(size + 2);
+    this.perimeter = approximateHeartPerimeter(this.radius, HEART_PERIMETER_FACTOR);
   }
 }
 
